2e4a00323096a1fe291096ce064b6522e2a5c7c4
Florian Dendorfer <flo.dendorfer@gmail.com>
2018-10-23 09:33:23 +0200

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿
+//Initialize all materialize components
+$(document).ready(function(){
+  $('#absenceWindow').modal();
+  $('#absenceFrom').datepicker({format: 'dd.mm.yyyy'});
+  $('#absenceTo').datepicker({format: 'dd.mm.yyyy'});
+})
