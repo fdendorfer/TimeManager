5c35f91766630fd43cc1484bdd056a0a7637bff4
Florian Dendorfer <flo.dendorfer@gmail.com>
2018-12-13 16:54:38 +0100

    Dotnet core 2.2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,11 +4,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace TourManager.Pages {
-  public class IndexModel : PageModel {
-    public void OnGet() {
+namespace TourManager.Pages
+{
+  public class IndexModel : PageModel
+  {
 
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
+    [Display(Name = "Benutzername")]
+    public string Username { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+        return Page();
+
+      return RedirectToPage("/OwnTimes");
     }
   }
 }
